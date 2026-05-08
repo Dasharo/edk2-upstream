tianocore/edk2

FmpDevicePkg/DetectTestKeyDxe: Add
This DXE can be used to validate firmware update key in cases when
FmpDxe is not part of the firmware.

Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

FmpDevicePkg/DetectTestKeyDxe/DetectTestKeyDxe.c
@@ -1,0 +1,32 @@
+/** @file
+  Detects if PcdFmpDevicePkcs7CertBufferXdr contains a test key.
+
+  Copyright (c) 2026, 3mdeb Sp. z o.o. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Library/DetectTestKeyLib.h>
+
+/**
+
+  Check if PcdFmpDevicePkcs7CertBufferXdr contains a test key.
+
+  @param ImageHandle     The image handle.
+  @param SystemTable     The system table.
+
+  @retval  EFI_SUCEESS  Regardless of the outcome.
+
+**/
+EFI_STATUS
+EFIAPI
+EntryPoint (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  DetectTestKey ();
+
+  return EFI_SUCCESS;
+}

Dasharo/edk2-upstream

FmpDevicePkg/DetectTestKeyDxe/DetectTestKeyDxe.c
@@ -1,0 +1,32 @@
+/** @file
+  Detects if PcdFmpDevicePkcs7CertBufferXdr contains a test key.
+
+  Copyright (c) 2026, 3mdeb Sp. z o.o. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Library/DetectTestKeyLib.h>
+
+/**
+
+  Check if PcdFmpDevicePkcs7CertBufferXdr contains a test key.
+
+  @param ImageHandle     The image handle.
+  @param SystemTable     The system table.
+
+  @retval  EFI_SUCCESS  Regardless of the outcome.
+
+**/
+EFI_STATUS
+EFIAPI
+EntryPoint (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  DetectTestKey ();
+
+  return EFI_SUCCESS;
+}